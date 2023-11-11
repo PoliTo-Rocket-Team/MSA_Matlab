294f6a80c37e434102b125797f280b07297995ce
Giuseppe Barbera <77114095+GiuseppeBarbera988@users.noreply.github.com>
2023-11-12 00:04:59 +0100

    Added plot with rocketpy simulations

--- EUROC Flight Analysis/flightDATA.m
+++ EUROC Flight Analysis/flightDATA.m
@@ -37,7 +37,7 @@ xlabel('time(S)')
 grid on
 legend('Location','northeast')
 
-%% ALTITUDE COMPARISON VEGA AND EASYMINI
+%% ALTITUDE COMPARISON VEGA EASYMINI and SIMULATION
 clc; clear all; close all;
 
 clc; 
@@ -46,23 +46,27 @@ close all;
 
 load("vegaDATA")
 load("easyDATAwV")
+load("RocketPyData")
 
 %VEGA
 timeV = (VEGAdata(2:end,1)-VEGAdata(2,1))/10;  %removing offset, time in seconds
 altitudeV = VEGAdata(2:end,2);
-velocityV = VEGAdata(2:end,3);
 display("apogee vega")
 max(altitudeV)
 
 %EASYMINI
 timeE = EASYdata(32:end,1);
-accelerationE = EASYdata(32:end,2);
 altitudeE = EASYdata(32:end,3);
-velocityE = EASYdata(32:end,4);
-voltageD = EASYdata(32:end,5);
-voltageM = EASYdata(32:end,6);
 display("apogee easymini")
 max(altitudeE)
+
+%ROCKETPY
+time_s = Rocketpydata(2:end,1);
+ground_level =  Rocketpydata(2,2)
+altitude_s = Rocketpydata(2:end,2)-ground_level;
+
+display("apogee RocketPy")
+max(altitude_s)
 
 ylabel("Altitude(m)")
 xlabel("Time(s)")
@@ -70,59 +74,92 @@ xlabel("Time(s)")
 plot(timeE,altitudeE,'b','DisplayName','altitude Easymini');
 hold on
 plot(timeV,altitudeV,'r','DisplayName','altitude Vega');
-hold off
+hold on
+plot(time_s,altitude_s,'k','DisplayName','altitude RocketPy');
 grid on
 legend('Location','northeast')
 
-%% VELOCITY COMPARISON BETWEEN VEGA AND EASYMINI
-clc; clear all; close all;
-
+%% VELOCITY COMPARISON BETWEEN VEGA EASYMINI AND ROCKETPY
 clc; 
 clear all;
 close all;
 
 load("vegaDATA")
 load("easyDATAwV")
+load("RocketPyData")
 
 %VEGA
 timeV = (VEGAdata(1:end,1)-VEGAdata(1,1))/10;  %removing offset, time in seconds
-altitudeV = VEGAdata(1:end,2);
 velocityV = VEGAdata(1:end,3);
-display("velocity vega")
-max(velocityV)
+
+
+
+%EASYMINI
+timeE = EASYdata(32:end,1);
+velocityE = EASYdata(32:end,4);
+
+%ROCKETPY
+time_s = Rocketpydata(2:end,1);
+velocity_s = Rocketpydata(2:end,3);
+
+display("velocity simulated with RocketPy")
+max(velocity_s)
+
+display("max velocity easymini")
+max(velocityE)
+
+
+
+plot(timeE,velocityE,'b','LineWidth',1.5,'DisplayName','velocity Easymini')
+hold on
+plot(timeV,velocityV,'k','LineWidth',1.5,'DisplayName','velocity Vega')
+hold on
+plot(time_s,velocity_s,'r','LineWidth',1.5,'DisplayName','velocity simulated by RocketPy')
+hold off
+grid on
+legend('Location','northeast')
+
+ylabel("velocity (m/s)")
+xlabel("Time(s)")
+
+
+
+
+%% ACCELERATION COMPARISON BETWEEN EASYMINI AND ROCKETPY
+clc; 
+clear all;
+close all;
+
+load("easyDATAwV")
+load("RocketPyData")
 
 %EASYMINI
 timeE = EASYdata(32:end,1);
 accelerationE = EASYdata(32:end,2);
-altitudeE = EASYdata(32:end,3);
-velocityE = EASYdata(32:end,4);
-voltageD = EASYdata(32:end,5);
-voltageM = EASYdata(32:end,6);
-display("max velocity easymini")
-max(velocityE)
-display("max acceleration easymini")
+
+%ROCKETPY
+time_s = Rocketpydata(2:end,1);
+acceleration_s = Rocketpydata(2:end,4);
+
+display("max acceleration rocketpy");
+max(acceleration_s/9.81)
+display("max acceleration easymini");
 max(accelerationE)
 
-
-
-
-plot(timeE,velocityE,'b','DisplayName','velocity Easymini')
+relative_error = (abs(max(acceleration_s)-max(accelerationE))/max(acceleration_s))*100
+plot(timeE,accelerationE,'b','LineWidth',1.5,'DisplayName','acceleration Easymini')
 hold on
-% plot(timeE,accelerationE,'k','LineWidth',1.5,'DisplayName','Acceleration Easymini')
-% hold on
-% plot(timeV,velocityV,'r','DisplayName','velocity Vega')
-% hold off
+plot(time_s,acceleration_s,'r--','LineWidth',1.5,'DisplayName','acceleration simulated by RocketPy')
+hold off
+
 grid on
 legend('Location','northeast')
 
-ylabel("Acceleration (m/s2)")
+ylabel("Acceleration (m/s^2)")
 xlabel("Time(s)")
 
 %% MAIN AND APOGEE VOLTAGES EASYMINI
-
 clc; clear all; close all;
-
-
 
 load("vegaDATA")
 load("easyDATAwV")
@@ -199,6 +236,7 @@ clc; clear all; close all;
 load("easyDATAwV")
 load("Boost_Data")
 load("acceleration_SIM")
+
 %Mass of the rocket
 
 %dry_mass
@@ -270,32 +308,7 @@ plot(timetot_SIM,DragVector_SIM','b','LineWidth',1.5,'DisplayName','Simulated Dr
 grid minor
 legend("location","northeast")
 
-%%
-clc
-clear all
-close all
 
-load("OpenRocket_BoostData.mat")
-load('easyDATAwV.mat')
-
-%dry_mass
-m_0 = 8.219;
-%wet mass
-m = 10.132;
-
-time_EM = (linspace(0,3.80,380))';
-acc_EM = EASYdata(2:381,2);
-thrust_s = spline(time,thrust,time_EM);
-thrust_s = [thrust_s(8:end) ; zeros(7,1)];
-% length(time_EM)
-% length(acc_EM)
-% length(thrust_s)
-
-f_mass = (m-m_0)/3.8;
-mt = @(t) m - f_mass*t ;
-Drag_during_burnout =@(t)  thrust_s - mt(t).*(acc_EM+9.81);
-DragVector = Drag_during_burnout(time_EM); 
-plot(time_EM,DragVector,'k','LineWidth',1.5,'DisplayName',' Real Drag Force during boostphase and coasting')
 
 %% Comparison Acceleration Simulated and Real Acceleration Easymini
 
@@ -312,13 +325,14 @@ time_SIM = Acceleration_SIM(1:324,1)
 acceleration_SIM = Acceleration_SIM(1:324,2)*9.81
 
 %BURNOUT
-time = Boost_DATA(:,1)
-acceleration = Boost_DATA(:,3)
-acceleration_sim = Boost_DATA(:,4).*9.81
+time = Boost_DATA(:,1);
+acceleration = Boost_DATA(:,3);
+acceleration_sim = Boost_DATA(:,4).*9.81;
 
 acceleration = [acceleration',accelerationE']'
 acceleration_sim = [acceleration_sim',acceleration_SIM']'
-
+display("Maximum Acceleration Simulated")
+max(acceleration_sim)
 time_sim = [time',time_SIM']'
 time_Real = [time',timeE']'
 
@@ -329,3 +343,60 @@ plot(time_Real',acceleration','b','LineWidth',1.5,'DisplayName','Real accelerati
 legend("location","northeast")
 hold off
 grid on
+
+
+%% DRAG FORCE CALCULATION
+clc
+clear all
+close all
+
+load("OpenRocket_BoostData.mat")
+load('easyDATAwV.mat')
+load('RocketPyData')
+
+%dry_mass
+m_0 = 8.219;
+%wet mass
+m = 10.132;
+
+%BURNOUT
+time_EM = (linspace(0,3.80,380))';
+acc_EM = EASYdata(2:381,2);
+thrust_s = spline(time,thrust,time_EM);
+thrust_s = [thrust_s(8:end) ; zeros(7,1)];
+
+%COASTING
+timeE = EASYdata(404:2309,1);
+accelerationE = EASYdata(404:2309,2)
+
+f_mass = (m-m_0)/3.8;
+mt = @(t) m - f_mass*t ;
+Drag_during_burnout =@(t)  thrust_s - mt(t).*(acc_EM+9.81);
+DragVector = Drag_during_burnout(time_EM);  %during burnout
+
+Drag_coasting = - m_0*(accelerationE+9.81)
+
+time_s = Rocketpydata(2:349,1)
+drag_s = Rocketpydata(2:349,5)
+
+DragVector =[DragVector',Drag_coasting']'   %adding to DragVector the drag coasting
+timeE =[timeE',time_EM']'
+
+% for element = [1:size(DragVector)]
+%     if DragVector(element)<0
+%         DragVector(element)=0;
+%     end
+% end
+
+for element = [1:size(drag_s)]
+    if drag_s(element)<0
+        drag_s(element)=0;
+    end
+end
+
+plot(timeE,DragVector,'k','LineWidth',1.5,'DisplayName',' Real Drag Force during boostphase and coasting')
+hold on 
+plot(time_s,drag_s,'r','LineWidth',1.5,'DisplayName','  Drag Force simulated by RocketPy')
+hold off
+
+legend("location","northeast")
