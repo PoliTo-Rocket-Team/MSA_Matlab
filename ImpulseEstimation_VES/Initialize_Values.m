e36bc4be9f8e1515fb6171b3b7b47131aaf26312
Mattia Saba <mattia.saba2001@gmail.com>
2024-02-09 18:04:45 +0100

    Changes to Initialize values and simulink model to plot graphs for SA Cup report

--- ImpulseEstimation_VES/Initialize_Values.m
+++ ImpulseEstimation_VES/Initialize_Values.m
@@ -1,10 +1,12 @@
 clc
-clearvars -except k*
+clearvars -except k* s t
 close all
-thrust_table = readmatrix("AeroTech_O5500X-PS.csv");
-thrust = thrust_table(:,2);
-thrust_time = thrust_table(:,1);
-time = linspace(0,40,1000);
+% thrust_table = readmatrix("AeroTech_O5500X-PS.csv");
+% thrust = thrust_table(:,2);
+% thrust_time = thrust_table(:,1);
+thrust = s;
+thrust_time = t;
+time = linspace(0,28,1000);
 thrust_interp = interp1(thrust_time,thrust,time); %interpolate where needed
 thrust_interp(isnan(thrust_interp)) = 0;
 % impulse_avg = 40960; %code to create a basic customized thrust curve
@@ -25,11 +27,12 @@ thrust_interp(isnan(thrust_interp)) = 0;
 %     end
 % end
 thrust_ts = timeseries(thrust_interp,time);
-mass_diff = 9.8;
-mass_motor = 16.8;
-t_burn = 3.9;
+mass_diff = 3.693;
+mass_motor = 6.451;
+t_burn = 3.6;
 j = 1;
-for mass_struct = 14:2:30
+% for mass_struct = 15:2:30
+        mass_struct = 19.23;
         m_init = mass_struct + mass_motor;
         m_final = m_init - mass_diff;
         k = -mass_diff/t_burn;
@@ -42,7 +45,32 @@ for mass_struct = 14:2:30
         mass_ts = timeseries(mass,time);
         out = sim("ThreeDOF_Rocket_Simulator_v2.slx");
         heights = getElement(out.yout,'Altitude');
-        heights_val = heights.Values.Data;
+        Velocity = getElement(out.yout,'Velocity');
+        Acceleration = getElement(out.yout,'Acceleration');
+        time_heights = heights.Values.Time;
+        time_velocity = Velocity.Values.Time;
+        time_acceleration = Acceleration.Values.Time;
+        heights_val = heights.Values.Data - 1400; % 1400 initial altitude for SA Cup Launch
+        Velocity_val = Velocity.Values.Data;
+        Acceleration_val = Acceleration.Values.Data;
         km9_130_O5500(j) = max(heights_val);
         j = j+1;
-end
+%end
+
+figure 
+plot(time_heights,heights_val,"k","LineWidth",2)
+xlabel("Time")
+ylabel("Altitude")
+grid on
+
+figure 
+plot(time_heights,Velocity_val,"k","LineWidth",2)
+xlabel("Time")
+ylabel("Velocity")
+grid on
+
+figure
+plot(time_heights,Acceleration_val,"k","LineWidth",2)
+xlabel("Time")
+ylabel("Acceleration")
+grid on
