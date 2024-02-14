293aa025aad503c471f35059bd39fd5d2a22e26e
Mattia Saba <mattia.saba2001@gmail.com>
2024-02-14 23:58:15 +0100

    O3400 sim

--- ImpulseEstimation_VES/Initialize_Values.m
+++ ImpulseEstimation_VES/Initialize_Values.m
@@ -1,12 +1,12 @@
 clc
 clearvars -except k* s t
 close all
-% thrust_table = readmatrix("AeroTech_O5500X-PS.csv");
-% thrust = thrust_table(:,2);
-% thrust_time = thrust_table(:,1);
-thrust = s;
-thrust_time = t;
-time = linspace(0,28,1000);
+thrust_table = readmatrix("Cesaroni_21062O3400-P.csv");
+thrust = thrust_table(:,2);
+thrust_time = thrust_table(:,1);
+% thrust = s;
+% thrust_time = t;
+time = linspace(0,40,1000);
 thrust_interp = interp1(thrust_time,thrust,time); %interpolate where needed
 thrust_interp(isnan(thrust_interp)) = 0;
 % impulse_avg = 40960; %code to create a basic customized thrust curve
@@ -27,9 +27,9 @@ thrust_interp(isnan(thrust_interp)) = 0;
 %     end
 % end
 thrust_ts = timeseries(thrust_interp,time);
-mass_diff = 3.693;
-mass_motor = 6.451;
-t_burn = 3.6;
+mass_diff = 10.930;
+mass_motor = 5.912;
+t_burn = 6.2;
 j = 1;
 % for mass_struct = 15:2:30
         mass_struct = 19.23;
@@ -50,7 +50,7 @@ j = 1;
         time_heights = heights.Values.Time;
         time_velocity = Velocity.Values.Time;
         time_acceleration = Acceleration.Values.Time;
-        heights_val = heights.Values.Data - 1400; % 1400 initial altitude for SA Cup Launch
+        heights_val = heights.Values.Data; % 1400 initial altitude for SA Cup Launch
         Velocity_val = Velocity.Values.Data;
         Acceleration_val = Acceleration.Values.Data;
         km9_130_O5500(j) = max(heights_val);
