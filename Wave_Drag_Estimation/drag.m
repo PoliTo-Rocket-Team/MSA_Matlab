44b29191d4bbc6d2a18ceef076286717b2b74afd
alexX1101 <aleconcorde1101@gmail.com>
2024-04-17 17:48:42 +0200

    Update drag.m

--- Wave_Drag_Estimation/drag.m
+++ Wave_Drag_Estimation/drag.m
@@ -3,9 +3,9 @@ clc
 close all
 
 %% DEFINITION AND SETUP OF VARIABLES
-mach = 1.7;
+mach = 2.0;
 air_density = 1.225;
-velocity = mach * sqrt(1.4 * 287 * air_density);
+velocity = mach * sqrt(1.4 * 287 * 288);
 [root_chord,tip_chord,span,thickness,sweep_angle,x_le,x_te_t,...
     x_te_r,c_1,c_2] = setup_variables; % look up the fun to change values
 
@@ -48,15 +48,22 @@ beta = sqrt(mach.^2 - 1);
 % f_0_w = @(x) interp1(x_interp,y_interp,x);
 % upper_bound = @(x) x - 3e-1;
 % lower_bound = @(x) x + 1e-1;
-x_integ = linspace(0,root_chord,5000);
-y_integ = linspace(0,root_chord,5000);
-[X,Y] = meshgrid(x_integ,y_integ);
-F_0 = final_integrand_0(X,Y,beta,300);
-F_2 = final_integrand_n(X,Y,beta,2,300);
-drag_out = (trapz(y_integ,trapz(x_integ,F_0,2)) + ...
-    trapz(y_integ,trapz(x_integ,F_2,2))) * ...
-    -(0.5 * air_density * velocity^2) / (2 * pi)
-
+% x_integ = linspace(0,root_chord,5000);
+% y_integ = linspace(0,root_chord,5000);
+% [X,Y] = meshgrid(x_integ,y_integ);
+% F_0 = final_integrand_0(X,Y,beta,300);
+% drag_out = trapz(y_integ,trapz(x_integ,F_0,2));
+% i = 2;
+% for n = 1:i
+%     F_N = final_integrand_n(X,Y,beta,2*n,300);
+%     drag_out = drag_out + 0.5*trapz(y_integ,trapz(x_integ,F_N,2));
+% end
+% drag_out = drag_out * (- 0.5 * air_density * velocity^2) / (2 * pi)
+theta = linspace(0,2*pi,300);
+y = final_integrand(theta,beta) * ...
+    -(0.5 * air_density * velocity^2) / (2 * pi);
+drag_w = trapz(theta,y) / (2 * pi);
+c_d_w = drag_w / (0.5 * air_density * velocity^2 * 0.013273)
 
 %% setup of the variables needed for various functions
 function [root_chord,tip_chord,span,thickness,sweep_angle,x_le,x_te_t,...
@@ -64,7 +71,7 @@ function [root_chord,tip_chord,span,thickness,sweep_angle,x_le,x_te_t,...
     root_chord = 0.355; % in m
     tip_chord = 0.194; % in m
     span = 0.085; % in m
-    thickness = 0.08; % in m
+    thickness = 0.008; % in m
     sweep_angle = 60; % in deg
     root_leading_edge_ratio = 0.2;
     root_trailing_edge_ratio = 0.2;
@@ -128,6 +135,19 @@ function sw = elemental_area_integrand_0(x,theta,beta)
 end
 
 
+function d2f = d2_elemental_area(x,theta,beta)
+    [~,~,span] = setup_variables;
+    elemental_area = @(x,theta_p,beta) integral(@(y) thickness_wing(x + ...
+        beta * y * cos(theta_p), y), 0, span);
+    f = zeros(1,length(x));
+    for i = 1:length(x)
+        f(i) = elemental_area(x(i),theta,beta);
+    end
+    df = gradient(f,x);
+    d2f = gradient(df,x);
+end
+
+
 function sw = elemental_area_integrand_n(x,theta,beta,n)
     [~,~,span] = setup_variables;
     elemental_area = @(x,theta_p,beta) integral(@(y) thickness_wing(x + ...
@@ -170,7 +190,7 @@ end
 
 
 function d2f = d2F_n_w(x,beta,n)
-    F_0_w_f = @(x,beta) (1 / (2 * pi)) * integral(@(theta) ...
+    F_0_w_f = @(x,beta) (1 / pi) * integral(@(theta) ...
     elemental_area_integrand_n(x,theta,beta,n), 0, 2 * pi);
     f = zeros(1,length(x));
     for i = 1:length(x)
@@ -274,5 +294,34 @@ function res = final_integrand_n(x_1,x_2,beta,n,n_int)
                 res(i,j) = -100;
             end
         end
+    end
+end
+
+
+function res = another_integrand(x_1,x_2,theta,beta,n_int)
+    [root_chord] = setup_variables;
+    x_interp = linspace(0,root_chord,n_int);
+    y_interp = d2_elemental_area(x_interp,theta,beta);
+    d2f_int = @(x) interp1(x_interp,y_interp,x);
+    integrand = @(x_1,x_2) d2f_int(x_1).*d2f_int(x_2).*log(abs(x_1 - x_2));
+    res = integrand(x_1,x_2);
+    for i = 1:length(res(:,1))
+        for j = 1:length(res(1,:))
+            if isnan(res(i,j)) || res(i,j) < -20
+                res(i,j) = -20;
+            end
+        end
+    end
+end
+
+
+function res = final_integrand(theta,beta)
+    [root_chord] = setup_variables;
+    res = zeros(1,length(theta));
+    x_integ = linspace(0,root_chord,1000);
+    [X_1,X_2] = meshgrid(x_integ,x_integ);
+    for i = 1:length(theta)
+        F = another_integrand(X_1,X_2,theta(i),beta,300);
+        res(i) = trapz(x_integ,trapz(x_integ,F,2));
     end
 end
