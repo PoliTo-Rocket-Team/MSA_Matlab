f2f9de3eb4c8221c6b0f29c3ca349c2cf465fd20
mattiasaba01 <mattia.saba2001@gmail.com>
2024-05-11 16:50:05 +0200

    Fixes to Delft Parachute loads

--- MainParachutes_Loads/ODEsolver.m
+++ MainParachutes_Loads/ODEsolver.m
@@ -1,38 +1,38 @@
 close all
-tspan = [0 0.7];
+clc
+clear all
+
+Do = 66/39.37;
+Vs = 24.7;
+t_f = 8*Do/Vs^0.9;
+
+tspan = [0 t_f];
 %y(1)--> v, y(2)--> m_a, y(3)--> F, y(4)--> CdS
-y0 = [27.379 0 0 0];
+y0 = [Vs 0 0 0.1];
 [t,y] = ode45(@(t,y) odefun(t,y), tspan, y0);
-
-
 
 rho = 1.19934; %Calculation of rho at 270 meters predicted opening of the main chute + 20 meters of ask 't harde
 m_p = 0.135;
-m_V = 18.640;
+m_V = 15.801;
 p = 0.2;
 CdS = 4.8557;
-CdS_d = 0.2467;
-C_Dr = 0.4523;
-S_r = 1.2456; %2pi r *l
+CdS_d = 0.4669;
+C_Dr = 0.5;
+S_r = 0.01327; %2pi r *l
 CdS_r = C_Dr * S_r;
 k_g = 1.068*(1-1.465*p-0.25975*p^2+1.2626*p^3);
 k_a = ((4/3)*pi*0.6^3*k_g)/((CdS)^(3/2));
 W_V = m_V*9.81;
 W_p = m_p*9.81;
-Do = 66/39.37
-Vs = 24.70
-t_f = 8*Do/Vs^0.9
-
-
-
 
 y_0 = [y(end,1) y(end,2) y(end,3) y(end,4)];
-Tspan = [0.7 10];
+Tspan = [t_f 10];
 [t_1,y_1] = ode45(@(t,y) odefun3(t,y), Tspan, y_0);
 t = [t;t_1];
 y = [y;y_1];
 
-acc = (y(:,3) + 0.5*rho.*y(:,1).^2*(CdS_r+CdS_d)-W_V)/m_V
+acc = (y(:,3) + 0.5.*rho.*(y(:,1).^2).*(CdS_r+CdS_d) - W_V)./m_V;
+
 figure
 plot(t,y(:,1),"Color","red")
 title("velocity")
@@ -62,3 +62,4 @@ plot(t,acc,"color","k")
 title("Acceleration")
 xlabel("t(s)")
 ylabel("v'(m/s^2)")
+grid on
--- MainParachutes_Loads/odefun3.m
+++ MainParachutes_Loads/odefun3.m
@@ -4,21 +4,18 @@ m_p = 0.135;
 m_V = 15.801;
 p = 0.2;
 CdS = 4.8557;
-CdS_d = 0.2467;
-C_Dr = 0.4523;
-S_r =1.2456; %pi r^2 *l
+CdS_d = 0.4669;
+C_Dr = 0.5;
+S_r = 0.01327; %2pi r *l
 CdS_r = C_Dr * S_r;
 k_g = 1.068*(1-1.465*p-0.25975*p^2+1.2626*p^3);
 k_a = ((4/3)*pi*0.6^3*k_g)/((CdS)^(3/2));
 W_V = m_V*9.81;
 W_p = m_p*9.81;
-Do = 66/39.37
-Vs = 24.359
-t_f = 8*Do/Vs^0.9
 
 dydt=zeros(4,1);
 dydt(4)= 0; %%CdS finale / filling time
-dydt(2)=(3/2).*rho.*k_a.*(y(4).^(1/2)).*(dydt(4));
+dydt(2)=(3/2).*rho.*k_a.*(y(4).^(1/2)).*(0);
 dydt(1)=-(y(3)+(0.5.*rho.*y(1).^2.*(CdS_r+CdS_d))-W_V)./m_V;
 dydt(3) = 0.5.*rho.*(2.*y(1).*dydt(1).*y(4)+((y(1)).^2).*dydt(4))+dydt(1).*dydt(2)+0.75.*rho.*k_a.*((y(4)).^(-0.5)).*((dydt(4)).^2).*y(1)+dydt(2).*dydt(1)-(1/m_V).*(y(2)+m_p).*(dydt(3)+rho.*y(1).*dydt(1).*(CdS_r+CdS_d));
 
